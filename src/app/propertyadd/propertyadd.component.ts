4099408c071839a01a43101ab423cea0a9c6aa9e
omkarjadhav21 <omkarjadhav7110@gmail.com>
2017-11-16 19:27:36 +0530

    Angular Wizard/Stepper/Multi-step-Form

---
+++ src/app/propertyadd/propertyadd.component.ts
@@ -0,0 +1,64 @@
+import { NgForm } from '@angular/forms/src/directives/ng_form';
+import { Component, OnInit } from '@angular/core';
+import { HostBinding } from '@angular/core/src/metadata/directives';
+
+@Component({
+  selector: 'app-propertyadd',
+  templateUrl: './propertyadd.component.html',
+  styleUrls: ['./propertyadd.component.scss']
+})
+export class PropertyaddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+prev1=this.fun1();
+  tab1:boolean;
+  tab2:boolean
+  tab3:boolean
+  tab4:boolean
+  tab5:boolean
+
+fun1(){
+  this.tab1=true;
+  this.tab2=false
+  this.tab3=false
+  this.tab4=false
+  this.tab5=false
+  console.log("in fun1111");
+}
+fun2(){
+  this.tab1=false;
+  this.tab2=true
+  this.tab3=false
+  this.tab4=false
+  this.tab5=false
+  console.log("in fun1111");
+}
+fun3(){
+  this.tab1=false;
+  this.tab2=false
+  this.tab3=true
+  this.tab4=false
+  this.tab5=false
+  console.log("in fun1111");
+}
+fun4(){
+  this.tab1=false;
+  this.tab2=false
+  this.tab3=false
+  this.tab4=true
+  this.tab5=false
+  console.log("in fun1111");
+}
+SubmitProperty(subProprtyFrm:NgForm){
+  this.tab1=false;
+  this.tab2=false
+  this.tab3=false
+  this.tab4=false
+  this.tab5=true
+  console.log(subProprtyFrm.value);
+}
+
+}
